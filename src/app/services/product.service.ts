e1f2ec37c5171f45dde4bdfb6abb1308d53e1717
ldex <laurent.duveau+git@gmail.com>
2022-06-10 08:47:39 -0400

    Gestion des Erreurs

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -23,7 +23,25 @@ export class ProductService {
                         .get<Product[]>(this.baseUrl)
                         .pipe(
                           tap(console.table),
-                          delay(1500) // pour démo...
+                          delay(1500), // pour démo...
+                          catchError(this.handleError)
                         );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    // in a real world app, you may send the error to the server using some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMsg: string;
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMsg = 'An error occurred:' + error.error.message;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      errorMsg = `Backend returned code ${error.status}, body was: ${error.error}`;
+    }
+    console.error(errorMsg);
+    // return an observable with a user-facing error message
+    return throwError(() => 'Something bad happened; please try again later.');
   }
 }
